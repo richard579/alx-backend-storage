20175e66f4b0e4729bfa08c41a7d963f83831687
richard579 <richardbeng358@outlook.com>
2024-01-18 13:43:56 +0000

    100-average_weighted_score.sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -9,7 +9,7 @@ BEGIN
 	SUM(corrections.score * projects.weight) / SUM(projects.weight)
 	FROM corrections
 	INNER JOIN projects
-	ON projects.id = corrections.projects_id
+	ON projects.id = corrections.project_id
 	WHERE corrections.user_id = user_id)
 	WHERE users.id = user_id;
 END $$
