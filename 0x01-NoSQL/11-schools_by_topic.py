d10992ad764ad15c0fb1fa7f116f4e3a8f943055
richard579 <richardbeng358@outlook.com>
2024-01-22 13:26:45 -0800

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -2,7 +2,7 @@
 """ MongoDB Operations using pymongo """
 
 
-def school_by_topic(mongo_collecton, topic):
+def schools_by_topic(mongo_collecton, topic):
     """ returns the list of school having a specific topic """
     documents = mongo_collection.find({"topics": topic})
     return list(documents)
