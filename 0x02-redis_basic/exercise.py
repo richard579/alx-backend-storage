31ba69fb20f43e5fa566bbe8794d2f14d295cf55
richard579 <richardbeng358@outlook.com>
2024-01-24 17:33:01 +0000

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     """ Decorator for cache class method to tracks args
     """
